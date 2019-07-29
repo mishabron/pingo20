110567315f687ee5ccfe8fbe3276713abf5965b1
misha <misha.bronshteyn@gmail.com>
2019-07-29 16:26:22 -0400

    improved free game activity

--- app/src/main/java/com/mbronshteyn/pingo20/activity/FreeGameActivity.java
+++ app/src/main/java/com/mbronshteyn/pingo20/activity/FreeGameActivity.java
@@ -1,9 +1,11 @@
 package com.mbronshteyn.pingo20.activity;
 
-import android.app.Activity;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -20,14 +22,22 @@ public class FreeGameActivity extends PingoActivity{
         setContentView(R.layout.activity_free_game);
 
         ImageView freeGameValoon = (ImageView) findViewById(R.id.freegame_baloon);
+        Glide.with(this).load(R.drawable.freegame_baloon_normal).diskCacheStrategy( DiskCacheStrategy.NONE )
+                .skipMemoryCache( true ).into(freeGameValoon);
+        ImageView glass = (ImageView) findViewById(R.id.glass);
 
         //pop up baloon after first delay
         new Handler().postDelayed(()->{
+            glass.setVisibility(View.VISIBLE);
+            Animation zoomIntAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.zoom_inglass);
+            glass.startAnimation(zoomIntAnimation);
+
             popBaloon(freeGameValoon);
         },1500);
 
         //start baloon animation after second delay
         new Handler().postDelayed(()->{
+            glass.setVisibility(View.INVISIBLE);
             freeGameValoon.setImageDrawable(getResources().getDrawable(R.drawable.freegame_animation,null));
             AnimationDrawable freeGameAnimation = (AnimationDrawable) freeGameValoon.getDrawable();
             long totalDuration = 0;
