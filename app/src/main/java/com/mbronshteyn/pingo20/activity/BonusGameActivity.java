25e7523c1046e3d724ee8dcf5268b5fe8e8993fb
misha <misha.bronshteyn@gmail.com>
2020-03-29 22:09:35 -0400

    added counter button

--- app/src/main/java/com/mbronshteyn/pingo20/activity/BonusGameActivity.java
+++ app/src/main/java/com/mbronshteyn/pingo20/activity/BonusGameActivity.java
@@ -1,5 +1,8 @@
 package com.mbronshteyn.pingo20.activity;
 
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
@@ -12,13 +15,17 @@ import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.mbronshteyn.pingo20.R;
-import com.mbronshteyn.pingo20.model.Game;
 
 import java.util.HashMap;
 
 public class BonusGameActivity extends PingoActivity {
 
-    private HashMap<Object, Object> buttonMap;
+    private HashMap<Object, Integer> buttonMap;
+    private int attemptCounter =5;
+    private AnimatorSet mSetRightOut;
+    private AnimatorSet mSetLeftIn;
+    private Button buttonCounter1;
+    private Button buttonCounter2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +60,17 @@ public class BonusGameActivity extends PingoActivity {
         buttonMap.put(4,R.drawable.button4);
         buttonMap.put(5,R.drawable.button5);
 
+        mSetRightOut = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.anim.out_animation);
+        mSetLeftIn = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.anim.in_animation);
+
+        buttonCounter1 = (Button) findViewById(R.id.counterButton1);
+        buttonCounter2 = (Button) findViewById(R.id.counterButton2);
+
+        int distance = 8000;
+        float scale = getResources().getDisplayMetrics().density * distance;
+        buttonCounter1.setCameraDistance(scale);
+        buttonCounter2.setCameraDistance(scale);
+
     }
 
     private void transitionToPlay() {
@@ -60,6 +78,63 @@ public class BonusGameActivity extends PingoActivity {
         Glide.with(this).load(R.drawable.bonuspin_background_play).into(backgroundView);
         ImageView playText = (ImageView) findViewById(R.id.playTextBackground);
         playText.setVisibility(View.INVISIBLE);
+
+        buttonCounter1.setVisibility(View.VISIBLE);
+        buttonCounter2.setVisibility(View.VISIBLE);
+
+        buttonCounter1.setBackground(getResources().getDrawable(buttonMap.get(attemptCounter),this.getTheme()));
+
+        buttonCounter1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playSound(R.raw.button);
+                if(attemptCounter >0) {
+                    buttonCounter1.setEnabled(false);
+                    buttonCounter2.setEnabled(true);
+                    flippToCounter(attemptCounter--,buttonCounter1,buttonCounter2);
+                }
+            }
+        });
+
+        buttonCounter2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playSound(R.raw.button);
+                if(attemptCounter >0) {
+                    buttonCounter2.setEnabled(false);
+                    buttonCounter1.setEnabled(true);
+                    flippToCounter(attemptCounter--,buttonCounter2,buttonCounter1);
+                }
+            }
+        });
+    }
+
+    public void flippToCounter(int counter, Button from, Button to) {
+
+        //flip button
+        mSetRightOut.setTarget(from);
+        to.setBackground(getResources().getDrawable(buttonMap.get(attemptCounter),this.getTheme()));
+        mSetLeftIn.setTarget(to);
+        mSetLeftIn.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+            }
+        });
+        mSetRightOut.start();
+        mSetLeftIn.start();
     }
 
     private void scaleUi() {
@@ -123,6 +198,19 @@ public class BonusGameActivity extends PingoActivity {
         ViewGroup.LayoutParams buttonParamsFinger = fingerButton.getLayoutParams();
         buttonParamsFinger.height = fingerButtonHeight;
         buttonParamsFinger.width = fingerButtonWidt;
+
+        //scale counter  button1
+        Button counterButton1 = (Button) findViewById(R.id.counterButton1);
+        int buttonSizeGo = (int) (newBmapHeight * 0.2406F);
+        ViewGroup.LayoutParams buttonParamsCounter1 = counterButton1.getLayoutParams();
+        buttonParamsCounter1.height = buttonSizeGo;
+        buttonParamsCounter1.width = buttonSizeGo;
+
+        //scale counter  button2
+        Button counterButton2 = (Button) findViewById(R.id.counterButton2);
+        ViewGroup.LayoutParams buttonParamsCounter2 = counterButton2.getLayoutParams();
+        buttonParamsCounter2.height = buttonSizeGo;
+        buttonParamsCounter2.width = buttonSizeGo;
 
     }
 
